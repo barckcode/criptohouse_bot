04eb147700e74107298df8862646d1f371d8631d
BarckCode <barckcode@gmail.com>
2021-10-06 19:09:16 +0200

    Setup Basic Bot for Telegram

---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
+
+
+updater = Updater(
+    token=os.environ['TOKEN_BOT'],
+    use_context=True
+)
+dispatcher = updater.dispatcher
+
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+def echo(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text
+    )
+
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
+
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=text_caps
+    )
+
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+
+def unknown(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command."
+    )
+
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+
+if __name__ == '__main__':
+    updater.start_polling()
